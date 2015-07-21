f9bcf1dcdf64a1204f3fdbfb3c04d6e35817257e
Simon <simongle@gmail.com>
2015-07-21 11:03:38 -0400

    added password length requirement and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -38,4 +38,14 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "password should be present and nonblank" do
+    @user.password = @user.password_confirmation = " " * 5
+    assert_not  @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not  @user.valid?
+  end
+
 end
