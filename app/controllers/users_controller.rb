7f86223dfe72006d4190febd11a8ce9afbb45234
Simon <simongle@gmail.com>
2015-07-29 12:39:37 -0400

    added signup form, error handling and tests for data persistence

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_parameters)
+    if @user.save
+      # Successful new user
+      redirect_to @user
+      flash[:success] = "Welcome to Figaro!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_parameters
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
