967152ccf5c3eeec6342739c6439861d7328ca08
Simon <simongle@gmail.com>
2015-08-25 14:56:53 -0400

    added role column to users database with default value as barber

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825181858) do
+ActiveRecord::Schema.define(version: 20150825185119) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.string   "password_digest"
     t.string   "remember_digest"
     t.boolean  "admin",           default: false
+    t.string   "role",            default: "barber"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
