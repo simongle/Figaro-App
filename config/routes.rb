81c61309e186472a867cb574d8465f58967aba46
Simon <simongle@gmail.com>
2015-08-26 17:29:30 -0400

    added terms of service page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
+  get 'tos' => 'static_pages#tos'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
